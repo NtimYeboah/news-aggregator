a9324af2e66274d506f43ddf7f833ffeb31607e8
Ntim Yeboah <lylegendary@gmail.com>
2025-01-17 16:25:52 +0000

    Install Laravel Breeze

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Actions\NewsRetrievalHandler;
 use App\Source\SourceManager;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Vite::prefetch(concurrency: 3);
     }
 }
